05ec642fa3992a2823cdf134380100c1927b25bc
Nora.Hixson25 <Nora.Hixson25@2024-313-10.bi.local>
2023-09-28 12:26:22 -0400

    Completed Card Class

--- Cards/src/Card.java
+++ Cards/src/Card.java
@@ -65,34 +65,34 @@ public class Card {
 		switch(Suit){
 			case "Clubs":
 				return 0;
-				break;
+		
 			case "Diamonds":
 				return 1;
-				break;
+			
 			case "Hearts":
 				return 2;
-				break;
+			
 			case "Spades":
 				return 3;
-				break;
+			
 		}
 		return -1;
 	}
 	
-	private String suitIntStr(suit){
+	private String suitIntStr(int suit){
 		switch(suit){
 			case 0:
 				return "Clubs";
-				break;
+			
 			case 1:
 				return "Diamonds";
-				break;
+				
 			case 2:
 				return "Hearts";
-				break;
+			
 			case 3:
-				return Spades;
-				break;
+				return "Spades";
+				
 		}
 		
 		System.out.println("Invaild suit name");
@@ -103,43 +103,43 @@ public class Card {
 			switch(rank) {
 				case 1:
 					return "Ace";
-					break;
+					
 				case 2:
 					return "Two";
-					break;
+					
 				case 3:
 					return "Three";
-					break;
+				
 				case 4:
 					return "Four";
-					break;
+					
 				case 5:
 					return "Five";
-					break;
+					
 				case 6:
 					return "Six";
-					break;
+				
 				case 7:
 					return "Seven";
-					break;
+			
 				case 8:
 					return "Eight";
-					break;
+				
 				case 9:
 					return "Nine";
-					break;
+				
 				case 10:
 					return "Ten";
-					break;
+				
 				case 11:
 					return "Jack";
-					break;
+		
 				case 12:
 					return "Queen";
-					break;
+			
 				case 13:
 					return "King";
-					break;
+			
 				
 			}
 			return null;
@@ -149,43 +149,43 @@ public class Card {
 		switch(s){
 		case "Ace":
 			return 1;
-			break;
+
 		case "Two":
 			return 2;
-			break;
+	
 		case "Three":
 			return 3;
-			break;
+	
 		case "Four":
 			return 4;
-			break;
+	
 		case "Five":
 			return 5;
-			break;
+		
 		case "Six":
 			return 6;
-			break;
+
 		case "Seven":
 			return 7;
-			break;
+
 		case "Eight":
 			return 8;
-			break;
+	
 		case "Nine":
 			return 9;
-			break;
+
 		case "Ten":
 			return 10;
-			break;
+
 		case "Jack":
 			return  11;
-			break;
+	
 		case "Queen":
 			return 12;
-			break;
+	
 		case "King":
 			return 13;
-			break;
+	
 		}
 		
 		System.out.println();
@@ -194,16 +194,28 @@ public class Card {
 	
 	public boolean equals(Object other){
 		if(other instanceof Card) {
-			if(this.getRank() == other.getRank() && this.getSuit() == other.getSuit())
+			if(this.getRank() == ((Card) other).getRank() && this.getSuit() == ((Card) other).getSuit())
 				return true;
 		}
 		return false;
 	}
 	
+	public int compareTo(Card other){
+		if(this.Rank > other.getRank())
+			return 1; 
+		if(this.Rank < other.getRank())
+			return -1;
+		if(this.getSuitInt() > other.getSuitInt())
+			return 1;
+		if(this.getSuitInt() < other.getSuitInt())
+			return -1;
+		
+		return 0;
+	}
 	
 	
 	public static void main(String []args){
-		
+		System.out.println("Testing yo");
 	}
 	
 	
